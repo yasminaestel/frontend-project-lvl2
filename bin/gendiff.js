a96377f2b447dbbf6f69189fb0d11c329f581d69
yasminaestel <yasminaestel@gmail.com>
2022-04-24 17:04:42 +0300

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .version('0.1')
+  .description('Compares two configuration files and shows a difference.')
+
+program.parse();
