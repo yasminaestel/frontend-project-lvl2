d2b97ffe94165a6ab33bd6d3d3a850891c6ab64c
yasminaestel <yasminaestel@gmail.com>
2022-04-28 16:18:57 +0300

    fix bin

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,6 @@ const program = new Command();
 program
   .version('0.1')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
 
 program.parse();
