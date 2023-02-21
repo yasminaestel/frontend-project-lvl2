187534e01cd89dea39e37516539c6527d689844b
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-02-21 12:24:15 +0200

    modified:   bin/genDiff.js

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -9,5 +10,8 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
-program.parse(process.argv);
+program.parse();
