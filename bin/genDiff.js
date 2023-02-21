e85514b2d7e9bb35f6d628ff41ad03133216287d
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-02-21 11:04:23 +0200

    fix help

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
 
 program.parse(process.argv);
