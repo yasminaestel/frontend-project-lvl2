50f9e5ef9a0608c724da83455e7a069a8bba8226
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-02-21 16:58:38 +0200

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,20 @@
 import genDiff from '../src/index.js';
 import fs from 'fs';
-import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
 
 const fileJson1 = '../__fixtures__/file1.json';
 const fileJson2 = '../__fixtures__ /file1.json';
-const resultPath = path.resolve('../__fixtures__/resultJson.txt');
-const resultJson = fs.readFileSync(resultPath);
+
+const resultJson = await readFile('/../__fixtures__/resultJson');
 
 test('genDiffJson', () => {
   expect(genDiff(fileJson1, fileJson2)).toEqual(resultJson);
