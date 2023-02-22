c5f9671d525342aa90ce8ce900d4933065757a71
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-02-22 10:12:30 +0200

    fix test

--- src/readFile.js
+++ src/readFile.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname, path } from 'path';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
