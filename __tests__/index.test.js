81e45ce2166ccbc60bc29498c1f0b9beafd81f8d
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-02-23 15:43:28 +0200

    add test YAML

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,8 +4,14 @@ import readFile from '../src/readFile.js';
 const fileJson1 = '../__fixtures__/file1.json';
 const fileJson2 = '../__fixtures__/file2.json';
 
-const resultJson = await readFile('/../__fixtures__/resultJson.txt');
+const fileYml1 = '../__fixtures__/file1.yml';
+const fileYaml2 = '../__fixtures__/file2.yaml';
+
+const result = await readFile('/../__fixtures__/result.txt');
 
 test('genDiffJson', () => {
-  expect(genDiff(fileJson1, fileJson2)).toEqual(resultJson);
+  expect(genDiff(fileJson1, fileJson2)).toEqual(result);
+});
+test('genDiffYml', () => {
+  expect(genDiff(fileYml1, fileYaml2)).toEqual(result);
 });
