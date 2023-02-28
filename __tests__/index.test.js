7277432a127fb0ea81937c2d9b0ee32da3acfed2
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-02-28 19:55:59 +0200

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,10 +10,6 @@ const getPath = (fileName) => {
   return getFixturePath;
 };
 
-test('genDiffFlat', () => {
-  expect(genDiff(getPath('file1.json'), getPath('file2.json'))).toEqual(readFile(getPath('resultFlat.txt')));
-  expect(genDiff(getPath('file1.yml'), getPath('file2.yaml'))).toEqual(readFile(getPath('resultFlat.txt')));
-});
 test('genDiffTree', () => {
   expect(genDiff(getPath('file3.json'), getPath('file4.json'))).toEqual(readFile(getPath('resultTree.txt')));
   expect(genDiff(getPath('file3.yml'), getPath('file4.yaml'))).toEqual(readFile(getPath('resultTree.txt')));
