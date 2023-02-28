16e3d7755920d2a7b0748bf656ca29bc7e75cef1
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-02-28 19:44:50 +0200

    stylish in bin

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
+import formater from '../src/stylish.js';
 
 const program = new Command();
 
@@ -9,9 +10,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, stylish = formater) => {
+    console.log(genDiff(filepath1, filepath2, stylish));
   });
 
 program.parse();
