b8eac13eae2687236a03f5d7993195a682d68efa
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-03-03 11:29:25 +0200

    fix stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -8,30 +8,31 @@ const symbol = {
 };
 
 const stylish = (diff, level = 1) => {
-  const array = diff.map((infoObject) => {
+  const linesArray = diff.map((infoObject) => {
+    const buildLine = (value, status) => `${getSpacesStylish(level, 2)}${symbol[status]}${infoObject.key}: ${stringify(value, level + 1)}`;
     if (infoObject.status === 'hasValueObject') {
       const value = stylish(infoObject.value, level + 1);
-      return (`${getSpacesStylish(level, 2)}${symbol.bothNoChange}${infoObject.key}: ${stringify(value, level + 1)}`);
+      return buildLine(value, 'bothNoChange');
     }
-    const value = stringify(infoObject.value, level + 1);
     if (infoObject.status === 'bothNoChange') {
-      return (`${getSpacesStylish(level, 2)}${symbol.bothNoChange}${infoObject.key}: ${value}`);
+      return buildLine(infoObject.value, 'bothNoChange');
     }
     if (infoObject.status === 'bothChange') {
-      const line1 = `${getSpacesStylish(level, 2)}${symbol.onlyObject1}${infoObject.key}: ${stringify(infoObject.value1, level + 1)}`;
-      const line2 = `${getSpacesStylish(level, 2)}${symbol.onlyObject2}${infoObject.key}: ${stringify(infoObject.value2, level + 1)}`;
+      const line1 = buildLine(infoObject.value1, 'onlyObject1');
+      const line2 = buildLine(infoObject.value2, 'onlyObject2');
       const stringConcatenation = (`${line1}\n${line2}`);
       return stringConcatenation;
     }
     if (infoObject.status === 'onlyObject1') {
-      return (`${getSpacesStylish(level, 2)}${symbol.onlyObject1}${infoObject.key}: ${value}`);
+      return buildLine(infoObject.value, 'onlyObject1');
     }
     if (infoObject.status === 'onlyObject2') {
-      return (`${getSpacesStylish(level, 2)}${symbol.onlyObject2}${infoObject.key}: ${value}`);
+      return buildLine(infoObject.value, 'onlyObject2');
     }
-    return ('error');
+    throw new Error('Unknown status');
   });
-  const result = ['{', ...array, `${getBracketSpaces(level)}}`].join('\n');
+
+  const result = ['{', ...linesArray, `${getBracketSpaces(level)}}`].join('\n');
   return result;
 };
 
