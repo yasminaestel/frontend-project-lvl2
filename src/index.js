fbb8658b590fe930a94701391c9dd540bfef6ea3
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-03-03 12:10:25 +0200

    fixed lint

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import getDiffInForm from './formatters/index.js';
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const dataFile1 = readFile(filepath1);
   const dataFile2 = readFile(filepath2);
-  const extentionFile1 = filepath1.split('.').pop().toLowerCase();
-  const extentionFile2 = filepath2.split('.').pop().toLowerCase();
+  const extentionFile1 = filepath1.split('.').reverse()[0];
+  const extentionFile2 = filepath2.split('.').reverse()[0];
   const parsedFile1 = parse(extentionFile1, dataFile1);
   const parsedFile2 = parse(extentionFile2, dataFile2);
   const diff = compareObject(parsedFile1, parsedFile2);
--- src/stringify/stringifyForStylish.js
+++ src/stringify/stringifyForStylish.js
@@ -2,16 +2,13 @@ import { getSpacesStringify, getBracketSpaces } from '../getSpaces.js';
 
 const stringifyForStylish = (value, level) => {
   const iter = (data, depth) => {
-    let result = '';
     if (typeof data !== 'object' || data === null) {
-      result = (`${data}`);
-      return result;
+      return (`${data}`);
     }
     const entries = Object.entries(data);
     const array = entries.map(([key, meaning]) => (`${getSpacesStringify(depth)}${key}: ${iter(meaning, depth + 1)}`));
     const countSpec = getBracketSpaces(depth);
-    result = ['{', ...array, `${countSpec}}`].join('\n');
-    return result;
+    return ['{', ...array, `${countSpec}}`].join('\n');
   };
   return iter(value, level);
 };
