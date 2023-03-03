5dd03736fbbd9e10cab01a05f8775179e51bbf84
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-03-03 09:35:11 +0200

    fix plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,30 +1,27 @@
 import stringify from '../stringify/stringifyForPlain.js';
 
+const formatDiff = (node, ancestry = '') => node.flatMap((infoObject) => {
+  const newAncesty = ancestry === '' ? `${infoObject.key}` : `${ancestry}.${infoObject.key}`;
+  if (infoObject.status === 'hasValueObject') {
+    return formatDiff(infoObject.value, newAncesty);
+  }
+  if (infoObject.status === 'bothNoChange') {
+    return null;
+  }
+  if (infoObject.status === 'bothChange') {
+    return `Property '${newAncesty}' was updated. From ${stringify(infoObject.value1)} to ${stringify(infoObject.value2)}`;
+  }
+  if (infoObject.status === 'onlyObject1') {
+    return `Property '${newAncesty}' was removed`;
+  }
+  if (infoObject.status === 'onlyObject2') {
+    return `Property '${newAncesty}' was added with value: ${stringify(infoObject.value)}`;
+  }
+  throw new Error('Unknown type node');
+});
+
 const plain = (diff) => {
-  const iter = (node, ancestry = '') => {
-    const array = node.flatMap((infoObject) => {
-      const newAncesty = ancestry === '' ? `${infoObject.key}` : `${ancestry}.${infoObject.key}`;
-      if (infoObject.status === 'hasValueObject') {
-        return iter(infoObject.value, newAncesty);
-      }
-      if (infoObject.status === 'bothNoChange') {
-        return null;
-      }
-      if (infoObject.status === 'bothChange') {
-        return `Property '${newAncesty}' was updated. From ${stringify(infoObject.value1)} to ${stringify(infoObject.value2)}`;
-      }
-      if (infoObject.status === 'onlyObject1') {
-        return `Property '${newAncesty}' was removed`;
-      }
-      if (infoObject.status === 'onlyObject2') {
-        return `Property '${newAncesty}' was added with value: ${stringify(infoObject.value)}`;
-      }
-      return ('error');
-    });
-    return array;
-  };
-  const preResult = iter(diff);
-  const result = preResult.filter((element) => element !== null);
+  const result = formatDiff(diff).filter((element) => element !== null);
   return result.join('\n');
 };
 
