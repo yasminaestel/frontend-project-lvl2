6609b73f3c2da8fc63091bc3f6dda88a6191b8f1
Yasmin Hodak <yasmin@MacBook-Air-Jan.lan>
2023-03-03 15:03:07 +0200

    fix

--- src/stringify/stringifyForStylish.js
+++ src/stringify/stringifyForStylish.js
@@ -1,16 +1,13 @@
 import { getSpacesStringify, getBracketSpaces } from '../getSpaces.js';
 
 const stringifyForStylish = (value, level) => {
-  const iter = (data, depth) => {
-    if (typeof data !== 'object' || data === null) {
-      return (`${data}`);
-    }
-    const entries = Object.entries(data);
-    const array = entries.map(([key, meaning]) => (`${getSpacesStringify(depth)}${key}: ${iter(meaning, depth + 1)}`));
-    const countSpec = getBracketSpaces(depth);
-    return ['{', ...array, `${countSpec}}`].join('\n');
-  };
-  return iter(value, level);
+  if (typeof value !== 'object' || value === null) {
+    return (`${value}`);
+  }
+  const entries = Object.entries(value);
+  const arrayStr = entries.map(([key, meaning]) => (`${getSpacesStringify(level)}${key}: ${stringifyForStylish(meaning, level + 1)}`));
+  const countSpec = getBracketSpaces(level);
+  return ['{', ...arrayStr, `${countSpec}}`].join('\n');
 };
 
 export default stringifyForStylish;
